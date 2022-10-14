d6fcc948ba9587568c4f5daef2e89116a6424d37
Rajpopzein <kraj0132@gamil.com>
2022-10-14 17:21:01 +0530

    removed duplicate files

--- GraphQL_project/server/schema/schema.js
+++ GraphQL_project/server/schema/schema.js
@@ -12,10 +12,10 @@ var userdata = [
 
 //dummy data 2
 var hobbydata = [
-    {id:"1",title:'programming',description:"writting this that can be done by the computer"},
-    {id:"2",title:'Bike-Rider',description:"writting this that can be done by the computer"},
-    {id:"3",title:'Reading books',description:"writting this that can be done by the computer"},
-    {id:"4",title:'Hearing Audiobook',description:"writting this that can be done by the computer"}
+    {id:"1",title:'programming',description:"writting this that can be done by the computer",userId:"1"},
+    {id:"2",title:'Bike-Rider',description:"writting this that can be done by the computer",userId:"2"},
+    {id:"3",title:'Reading books',description:"writting this that can be done by the computer",userId:"4"},
+    {id:"4",title:'Hearing Audiobook',description:"writting this that can be done by the computer",userId:"2"}
 ]
 
 // dummy data 3
@@ -58,7 +58,13 @@ const HobbyType = new GraphQLObjectType(
         fields:()=>({
             id:{type:GraphQLID},
             title:{type:GraphQLString},
-            description:{type:GraphQLString}
+            description:{type:GraphQLString},
+            user:{
+                type:userType,
+                resolve(parent,args){
+                    return _.find(userdata,{id:parent.userId})
+                }
+            }
             
         })
     }
@@ -71,6 +77,7 @@ const postType = new GraphQLObjectType(
         fields:()=>({
             id:{type:GraphQLID},
             command:{type:GraphQLString},
+            //Will get user data
             users:{
                 type:userType,
                 resolve(parent,args)
