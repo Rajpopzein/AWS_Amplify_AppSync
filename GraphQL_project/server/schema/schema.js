9143466aa35b7a4e29071cdbfb2e3e4b27fd8b0c
Rajpopzein <kraj0132@gamil.com>
2022-10-14 11:18:38 +0530

    added department Type and accessed inside rootquery

--- GraphQL_project/server/schema/schema.js
+++ GraphQL_project/server/schema/schema.js
@@ -10,7 +10,19 @@ const userType = new GraphQLObjectType(
         fields:()=>({
             id:{type:GraphQLString},
             name:{type:GraphQLString},
-            age:{type:GraphQLInt}
+            age:{type:GraphQLInt},
+            departmentID:{type:GraphQLString}
+        })
+    }
+)
+
+const depatmentType = new GraphQLObjectType(
+    {
+        name: 'department',
+        description : 'Documentation for department',
+        fields:()=>({
+            departmentName:{type:GraphQLString},
+            departmentID:{type:GraphQLString}
         })
     }
 )
@@ -21,20 +33,27 @@ const rootquery = new GraphQLObjectType({
     description:'Description',
     fields:{
         user: {
-            type:userType,
+            type:userType,depatmentType,
+            //Argument need to getting data 
             args:{
                 id:{type:GraphQLString}
             },
-            
+
             resolve(parent,args)
             {
+                let department = {
+                    departmentID:"123456",
+                    departmentName:"InformationTechnology"
+                }
+
                 let user ={
                     name:"rajkumar.r",
                     age: 24,
-                    id : "1234"
+                    id : "1234",
+                    departmentID: department.departmentID
                 }
 
-                return user
+                return user 
                 //where we get data from api or frontend
                 //we resolve with data
                 //get and return data from data source
@@ -43,6 +62,7 @@ const rootquery = new GraphQLObjectType({
     }
 })
 
+//exporting GraphQlSchema with root query
 module.exports = new GraphQLSchema({
     query:rootquery
 })
